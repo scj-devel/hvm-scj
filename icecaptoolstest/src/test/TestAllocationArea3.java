d04e5f23a89337c32849ebc099ab8714f5116a6c
stephankorsholm <sek@viauc.dk>
2015-07-29 19:26:13 +0200

    skjhdkjashdjk

---
+++ icecaptoolstest/src/test/TestAllocationArea3.java
@@ -0,0 +1,74 @@
+package test;
+
+import vm.Memory;
+
+public class TestAllocationArea3 {
+
+	private static class ReusableString {
+		private static final int REUSABLESTRINGSIZE = 1000;
+		Memory stringMemory;
+
+		String string;
+
+		public ReusableString() {
+			stringMemory = Memory.allocateInHeap(REUSABLESTRINGSIZE);
+		}
+
+		public void set(ReusableStringInitializer initializer) {
+			Memory currentMemory = Memory.switchToArea(stringMemory);
+
+			stringMemory.reset(0);
+
+			string = initializer.initialize();
+
+			Memory.switchToArea(currentMemory);
+		}
+
+		public String get() {
+			return string;
+		}
+	}
+
+	private static class ReusableStringInitializer  {
+		public int number;
+
+		public String message;
+
+		public String initialize() {
+			return message + "*" + number;
+		}
+	}
+
+	public static void main(String[] args) {
+		ReusableString array[] = new ReusableString[4];
+
+		ReusableStringInitializer initializer = new ReusableStringInitializer();
+
+		for (int i = 0; i < array.length; i++) {
+			array[i] = new ReusableString();
+		}
+
+		Memory mainArea = Memory.getHeapArea();
+
+		int start = mainArea.consumedMemory();
+
+		int count = 0;
+		
+		while (count < 100) {
+			initializer.message = "info:";
+			initializer.number = count;
+			array[0].set(initializer);
+			devices.Console.println(array[0].get());
+			count++;
+		}
+
+		int end = mainArea.consumedMemory();
+		int consumed = (end - start);
+
+		devices.Console.println("Used " + consumed + " bytes");
+		
+		if (consumed == 0) {
+			args = null;
+		}
+	}
+}
