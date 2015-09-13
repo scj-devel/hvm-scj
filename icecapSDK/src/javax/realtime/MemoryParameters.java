06659817572ca7868daa739acaade0d4a7e0585c
HansSoendergaard <hso@viauc.dk>
2015-09-13 09:52:51 +0200

    HSO: class ConfigurationParameters, - new class in Draft

--- icecapSDK/src/javax/realtime/MemoryParameters.java
+++ icecapSDK/src/javax/realtime/MemoryParameters.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class MemoryParameters implements Cloneable, Serializable {
 
-	private static final long serialVersionUID = 123456789987654321L;
+	private static final long serialVersionUID = 123456789987654100L;
 	
 	protected long maxMemoryArea;
 	protected long maxImmortal;
@@ -12,9 +12,9 @@ public class MemoryParameters implements Cloneable, Serializable {
 	public static final long NO_MAX = -1L;
 
 	public MemoryParameters(long maxMemoryArea, long maxImmortal) {
-		if (maxMemoryArea < -1L)
+		if (maxMemoryArea <= -1L)
 			throw new IllegalArgumentException("maxMemoryArea not legal");
-		if (maxImmortal < -1L)
+		if (maxImmortal <= -1L)
 			throw new IllegalArgumentException("maxImmortal not legal");
 
 		this.maxMemoryArea = maxMemoryArea;
