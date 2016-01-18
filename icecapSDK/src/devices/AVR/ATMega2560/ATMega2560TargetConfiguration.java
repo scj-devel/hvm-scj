21310b4d25430c6265323320471a5dbd91e64e44
stephankorsholm <sek@viauc.dk>
2016-01-18 09:55:06 +0100

    fixing AOT compiler issue with programs without exception classes

--- icecapSDK/src/devices/AVR/ATMega2560/ATMega2560TargetConfiguration.java
+++ icecapSDK/src/devices/AVR/ATMega2560/ATMega2560TargetConfiguration.java
@@ -6,6 +6,30 @@ import util.BaseTargetConfiguration;
 
 public abstract class ATMega2560TargetConfiguration extends BaseTargetConfiguration implements TargetConfiguration {
 
+	@IcecapCVar(expression = "DDRA", requiredIncludes = "#include \"avr/io.h\"\n")
+	public static byte DDRA;
+	@IcecapCVar(expression = "PORTA", requiredIncludes = "#include \"avr/io.h\"\n")
+	public static byte PORTA;
+	@IcecapCVar(expression = "DDRB", requiredIncludes = "#include \"avr/io.h\"\n")
+	public static byte DDRB;
+	@IcecapCVar(expression = "PORTB", requiredIncludes = "#include \"avr/io.h\"\n")
+	public static byte PORTB;
+	@IcecapCVar(expression = "DDRC", requiredIncludes = "#include \"avr/io.h\"\n")
+	public static byte DDRC;
+	@IcecapCVar(expression = "PORTC", requiredIncludes = "#include \"avr/io.h\"\n")
+	public static byte PORTC;
+	@IcecapCVar(expression = "DDRD", requiredIncludes = "#include \"avr/io.h\"\n")
+	public static byte DDRD;
+	@IcecapCVar(expression = "PORTD", requiredIncludes = "#include \"avr/io.h\"\n")
+	public static byte PORTD;
+	@IcecapCVar(expression = "DDRE", requiredIncludes = "#include \"avr/io.h\"\n")
+	public static byte DDRE;
+	@IcecapCVar(expression = "PORTE", requiredIncludes = "#include \"avr/io.h\"\n")
+	public static byte PORTE;
+	@IcecapCVar(expression = "DDRF", requiredIncludes = "#include \"avr/io.h\"\n")
+	public static byte DDRF;
+	@IcecapCVar(expression = "PORTF", requiredIncludes = "#include \"avr/io.h\"\n")
+	public static byte PORTF;
 	@IcecapCVar(expression = "DDRG", requiredIncludes = "#include \"avr/io.h\"\n")
 	public static byte DDRG;
 	@IcecapCVar(expression = "PORTG", requiredIncludes = "#include \"avr/io.h\"\n")
@@ -32,4 +56,19 @@ public abstract class ATMega2560TargetConfiguration extends BaseTargetConfigurat
 	public int getJavaHeapSize() {
 		return 2048;
 	}
+	
+	protected static void blink(int i) {
+		DDRG |= 0x3;
+
+		while (true) {
+			PORTG ^= 0x3;
+			devices.System.delay(i);
+		}
+	}
+	
+	protected static int getReasonableProcessStackSize() {
+		return 256; /* 1 kB */
+	}
+	
+	
 }
