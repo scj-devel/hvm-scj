2e9277d186071e4c81a80b06919681cfdb5cb863
stephankorsholm <sek@viauc.dk>
2016-03-10 18:34:50 +0100

    adding configuration files for ATSAMe70

---
+++ icecapSDK/src/devices/arm/ATSAMe70/ATSAMe70MachineFactory.java
@@ -0,0 +1,37 @@
+package devices.arm.ATSAMe70;
+
+import vm.MachineFactory;
+import vm.RealtimeClock;
+import vm.SP;
+
+public class ATSAMe70MachineFactory implements MachineFactory {
+
+	@Override
+	public void initInterrupts() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public SP getProcessSP() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RealtimeClock getRealtimeClock() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void startSystemTick() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stopSystemTick() {
+		// TODO Auto-generated method stub
+
+	}
+}
